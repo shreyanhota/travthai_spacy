522641692f8cb651010edac3a6ad805b98394b34
Shreyan Hota <shreyanhota@gmail.com>
2024-08-28 14:29:25 +0700

    first commit

--- app_spacy.py
+++ app_spacy.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index_spacy.html')
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
